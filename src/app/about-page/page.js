bbe29d0dbfb812ea94fc6ac45210ef0fb74bec1e
nooralrawashdah <nooralrawashdah5@gmail.com>
2026-03-26 16:33:53 +0300

    first commit

---
+++ src/app/about-page/page.js
@@ -0,0 +1,14 @@
+import Navbar  from "@/components/Navbare";
+//import Body from "@/components/body";
+import Footer from "@/components/Footer";
+export default function AboutPage() {
+
+  return (
+  
+    <div>
+        <Navbar/>
+       <body/>
+        <Footer/>
+    </div>
+  )
+}
